774b38acedc161fae021ff965175961f04b733ce
Nathan C. Castle <nathan.castle@outlook.com>
2015-07-14 14:31:22 -0700

    First draft of forms list view samples

---
+++ UserInterface/ListView/BuiltInCells/builtInCellsListView/builtInCellsListView/builtInCellsListView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace builtInCellsListView
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			//MainPage = new ListViewCode ();
+			MainPage = new ListViewXaml ();
+		}
+
+		protected override void OnStart()
+		{
+
+
+		}
+
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ UserInterface/ListView/Grouping/groupingSampleListView/groupingSampleListView/groupingSampleListView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace groupingSampleListView
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// Adjust commenting to choose which method to use
+			//MainPage = new GroupedListCode ();
+			MainPage = new GroupedListXaml();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
